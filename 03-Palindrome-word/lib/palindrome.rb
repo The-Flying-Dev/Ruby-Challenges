bf5f96daa065c221a6a969246e51f8f79cbe8ab0
The-Flying-Dev <leandranm@outlook.com>
2023-05-07 22:35:09 +0200

    Exercise 03

--- 03-Palindrome-word/lib/palindrome.rb
+++ 03-Palindrome-word/lib/palindrome.rb
@@ -1,6 +1,6 @@
 def palindrome?(word)
   # TODO: return true/false depending if the *word* is a palindrome
-
+  return word.downcase == word.downcase.reverse
 end
 
 # To see the result of this method you can uncomment the line below:
