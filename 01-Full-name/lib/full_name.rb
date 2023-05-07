1238f295a9ae4a3fcf2df3d61cada0a7f2f5b5c6
The-Flying-Dev <leandranm@outlook.com>
2023-05-07 22:11:27 +0200

    Exercise 01

--- 01-Full-name/lib/full_name.rb
+++ 01-Full-name/lib/full_name.rb
@@ -1,6 +1,8 @@
 def full_name(first_name, last_name)
   # TODO: return the full name in a nice format
+  return "The names are the incorrect format" if !(first_name.is_a?(String) || last_name.is_a?(String))
 
+  return "#{first_name.downcase.capitalize} #{last_name.downcase.capitalize}"
 end
 
 # To see the result of this method you can uncomment the line below:
