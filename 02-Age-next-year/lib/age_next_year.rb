43f2db7da6415cef1d34a1cba5284e84c6f16c69
The-Flying-Dev <leandranm@outlook.com>
2023-05-07 22:29:07 +0200

    Exercise 02

--- 02-Age-next-year/lib/age_next_year.rb
+++ 02-Age-next-year/lib/age_next_year.rb
@@ -1,6 +1,6 @@
 def age_next_year(age)
   # TODO: return a message telling you how old you will be in 1 year.
-
+  return "Next year I'll be #{age += 1} years old"
 end
 
 # To see the result of this method you can uncomment the line below:
