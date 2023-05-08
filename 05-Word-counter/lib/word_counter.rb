29792f2accd8f04fe694a361c018a12174a01c4c
The-Flying-Dev <leandranm@outlook.com>
2023-05-08 12:29:11 +0200

    Exercise 05, wrote additional condition

--- 05-Word-counter/lib/word_counter.rb
+++ 05-Word-counter/lib/word_counter.rb
@@ -1,10 +1,12 @@
 def word_counter(sentence)
-  # TODO: return the number of words in the given sentence
+  # TODO: return the number of words in the given sentence 
 
-  word_count = sentence.split.count
+  return 0 if sentence.strip.length == 0
+
+  word_count = sentence.split.length
   return 0 if word_count == 0
   return word_count
-  
+
 end
 
 # To see the result of this method you can uncomment the line below:
