957591cd2986f25d0fda04b5247af82203f4d2c3
The-Flying-Dev <leandranm@outlook.com>
2023-05-08 13:17:03 +0200

    Exercise 08

---
+++ 08-Can-you-vote/lib/can_you_vote.rb
@@ -0,0 +1,9 @@
+def can_you_vote?(age)
+  # TODO: return true/false depending on the *age* given
+  
+  return age >= 18
+
+end
+
+# To see the result of this method you can uncomment the line below:
+# p can_you_vote?(18)
