589a74b6e5257530fcfb29ef9254a1d8990a609e
The-Flying-Dev <leandranm@outlook.com>
2023-05-08 13:14:18 +0200

    Exercise 07

---
+++ 07-odd-or-even/lib/odd_or_even.rb
@@ -0,0 +1,13 @@
+def odd_or_even(number)
+  # TODO: return "odd"/"even" depending on the *number*
+
+  if number % 2 == 0
+    return "even"
+  else 
+    return "odd"
+  end 
+  
+end
+
+# To see the result of this method you can uncomment the line below:
+# p odd_or_even(2)
